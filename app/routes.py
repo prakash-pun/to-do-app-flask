0c8bf121d747ab62b27c41edef9611e32b0de9c4
prakash-pun7 <punbishal777@gmail.com>
2020-09-17 20:56:24 +0545

    Update the functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,43 @@
-from app import app
-from flask import render_template
+from app import app, db
+from flask import render_template, request, flash, get_flashed_messages, redirect, url_for
+from datetime import datetime
+from app.forms import AddTaskForm, DeleteTaskForm
+from app.models import Task
 
-@app.route("/")
-@app.route("/home")
-@app.route("/index")
-def index():
-   return render_template('index.html')
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+   form = AddTaskForm()
+   tasks = None
+   if request.form:
+      try:
+         task = Task(title=form.title.data, date=datetime.utcnow())
+         db.session.add(task)
+         db.session.commit()
+         return redirect(url_for('home'))
+      except Exception as e:
+         print("Failed to add Task")
+         print(e)
+   tasks = Task.query.all()
+   return render_template('index.html', tasks=tasks)
+
+@app.route("/update", methods=["POST"])
+def update():
+   try:
+      newTask = request.form.get("newTask")
+      oldTask = request.form.get("oldTask")
+      task = Task.query.filter_by(title=oldTask).first()
+      task.title = newTask
+      db.session.commit()
+   except Exception as e:
+      print("Could not update book title")
+      print(e)
+   return redirect('/')
+
+@app.route("/delete", methods=["POST"])
+def delete():
+   title = request.form.get('task')
+   task = Task.query.filter_by(title=title).first()
+   db.session.delete(task)
+   db.session.commit()
+   return redirect('/')
--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 import os
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or b'6\xe9\xda\xead\x81\xf7\x8d\xbbH\x87\xe8m\xdd3%'
+
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+	
+	basedir = os.path.abspath(os.path.dirname(__file__))
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'task.db')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
